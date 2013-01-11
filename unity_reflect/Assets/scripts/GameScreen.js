59b6f5a444cbbd9a2b3e86d1276a0526d22aef78
stevesan <stevenan@gmail.com>
2013-01-11 00:09:11 -0800

    tweaked jump animation; new AlphaHierarchy system

--- unity_reflect/Assets/scripts/GameScreen.js
+++ unity_reflect/Assets/scripts/GameScreen.js
@@ -4,38 +4,26 @@
 //----------------------------------------
 #pragma strict
 
+@script RequireComponent(AlphaHierarchy)
+
 var game:GameController;
 var fadeInAnim = new ParameterAnimation();
 var fadeOutAnim = new ParameterAnimation();
 
 // If the game controller's state equals this, the sub-tree will be shown.
 var showingGameState = "startscreen";
-var recurseDepth = 1;
 
 private var state = "hidden";
 
-function SetAlphaRecursive(go:GameObject, alpha:float, depthLeft:int)
-{
-    var sprite = go.GetComponent( tk2dSprite );
-    if( sprite != null )
-        sprite.color.a = alpha;
-
-    if( go.guiText != null )
-        go.guiText.material.color.a = alpha;
-
-    if( depthLeft > 0 )
-    {
-        for( var child:Transform in go.transform )
-        {
-            SetAlphaRecursive(child.gameObject, alpha, depthLeft-1);
-        }
-    }
-}
-
 function Start ()
 {
-    SetAlphaRecursive(gameObject, 0.0, recurseDepth);
+    GetComponent(AlphaHierarchy).localAlpha = 0.0;
     state = 'hidden';
+
+    for( var child:Transform in transform )
+    {
+        child.gameObject.SetActive(false);
+    }
 }
 
 function Update ()
@@ -54,17 +42,33 @@ function Update ()
         {
             state = "fadein";
             fadeInAnim.Play();
+            GetComponent(AlphaHierarchy).localAlpha = 0.0;
+
+            for( var child:Transform in transform )
+            {
+                child.gameObject.SetActive(true);
+            }
         }
     }
     else if( state == "fadeout" )
     {
         fadeOutAnim.Update();
-        SetAlphaRecursive( gameObject, 1-fadeOutAnim.GetFraction(), recurseDepth );
+        GetComponent(AlphaHierarchy).localAlpha = 1.0 - fadeOutAnim.GetFraction();
+
+        if( fadeOutAnim.GetFraction() >= 1.0 )
+        {
+            state = "hidden";
+
+            for( var child:Transform in transform )
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
     }
     else if( state == "fadein" )
     {
         fadeInAnim.Update();
-        SetAlphaRecursive( gameObject, fadeInAnim.GetFraction(), recurseDepth );
+        GetComponent(AlphaHierarchy).localAlpha = fadeInAnim.GetFraction();
 
         if( game.GetState() != showingGameState )
         {
